68f4da8027de23e0cf21bc0bc422461fc59bcf60
Rodrigo Lozano <rlazano@applaudostudios.com>
2023-01-06 16:36:43 -0600

    update into angular 12

--- rickAndMorty/src/app/components/pages/characters/character-list/character-list.component.ts
+++ rickAndMorty/src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '@app/shared/interface/character.interface';
 import { CharacterService } from '@app/shared/services/character.service';
-// import { take } from 'rxjs/operators';
 
-// type RequestInfo = {
-//   next: string;
-// };
+type RequestInfo = {
+  next: string;
+};
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.scss'],
 })
 export class CharacterListComponent implements OnInit {
-  // info: RequestInfo = {
-  //   next: null,
-  // };
+  info: RequestInfo = {
+    next: null,
+  };
 
   characters: Character[] = [];
   private pageNum = 1;
@@ -25,17 +24,6 @@ export class CharacterListComponent implements OnInit {
   constructor(private characterSvc: CharacterService) {}
 
   ngOnInit(): void {
-    // this.getDataFromService();
   }
 
-  // private getDataFromService():void{
-  //   return this.characterSvc
-  //     .searchCharacters(this.query, this.pageNum)
-  //     .pipe(take(1))
-  //     .subscribe((res: any) => {
-  //       const { info, results } = res;
-  //       this.characters = [...this.characters, ...results];
-  //       this.info = info;
-  //     });
-  // }
 }
