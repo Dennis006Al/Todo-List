6193ad0a36fe6c45c59fd33bbdb43c9116c34edf
Rodrigo Lozano <rlazano@applaudostudios.com>
2023-01-12 10:51:11 -0600

    changes

--- reactive-forms/src/app/app.module.ts
+++ reactive-forms/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { MaterialModule } from './shared/modules/material/material.module';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
